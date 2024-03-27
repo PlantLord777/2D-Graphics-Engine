254b2e9f08afea44b18f3ef8834af04a80f5851d
PlantLord777 <johnjohnson55687@gmail.com>
2024-03-27 17:33:11 -0500

    update

--- README.md
+++ README.md
@@ -5,5 +5,9 @@ this is the rat game.
 How to Run:
 1. Download VSStudio 2022 with c++ compatablity
 2. Clone the ratworld repository
-3. Do not touch CMakeLists 
-4. Run
+3. Let cmake build (should be automatic)
+4. switch to runing ratworld.exe
+5. Run
+
+If you make changes push them to a seperate branch
+
--- rat.cpp
+++ rat.cpp
@@ -2,32 +2,49 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-
+using std::endl;
 int main()
 {
     std::cout << "rat";
-    glfwInit();
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Ratworld", NULL, NULL);
+    //initialize glfw
+    if (!glfwInit()) {
+        std::cout << "glfw failed"<<endl;
+        return -1;
+    }
+    
+    //set options
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    //initialize window
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Ratworld", NULL, NULL);
     if (!window)
     {
         std::cout << "fail" << std::endl;
         glfwTerminate();
-        exit(EXIT_FAILURE);
+        return(-1);
     }
 
-    
-
     glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
-
-    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
-    const GLubyte* version = glGetString(GL_VERSION); // version as a string
-    printf("Renderer: %s\n", renderer);
-    printf("OpenGL version supported %s\n", version);
-
-    // tell GL to only draw onto a pixel if the shape is closer to the viewer
-    glEnable(GL_DEPTH_TEST); // enable depth-testing
-    glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+    
+    glewExperimental = GL_TRUE;
 
 
+    //set background color
+    glClearColor(.3f,.3f,.3f,1.0f);
+    //loop till window close
+    while (!glfwWindowShouldClose(window))
+    {
+        //accept inputs
+        glfwPollEvents();
+        //clears buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 1;
 }
