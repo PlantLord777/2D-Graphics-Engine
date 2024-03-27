e06677f6d34baea5e1b3479fca8207e3e10d5abf
PlantLord777 <johnjohnson55687@gmail.com>
2024-03-27 11:28:51 -0500

    it works now

--- rat.cpp
+++ rat.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
+
 
 int main()
 {
     std::cout << "rat";
-    return 0;
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL Triangle", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "fail" << std::endl;
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    int rat=0;
+    std::cin >> rat;
+    std::cout << "rat2";
+
 }
