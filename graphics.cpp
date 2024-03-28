4c82c2d864a3b31fcb3f4c715fe88e1938eb84d4
PlantLord777 <johnjohnson55687@gmail.com>
2024-03-28 14:14:56 -0500

    main.cs can now access input from the canvas

--- graphics.cpp
+++ graphics.cpp
@@ -2,14 +2,35 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include < stdio.h > 
+#include <string.h>
+#include <windows.h>
+#include <atlbase.h>
+#include <atlconv.h>
 
 using std::endl;
+using std::string;
+
+GLFWwindow* w ;
+string lastkey="rat";
 //this is neccessary for export as dll
 extern "C" {
-    __declspec(dllexport) int add(int a, int b) {
-        return a + b;
+
+
+    __declspec(dllexport) void HandleKeyInput(GLFWwindow* w, int key, int status, int action, int mods) {
+        string k = "";
+        k += key;
+        k += " ";
+        k += status;
+        k += " ";
+        k += action;
+        k += " ";
+        k += mods; 
+        lastkey = k;
+        
+
     }
 
+  
 
     __declspec(dllexport) int rat()
     {
@@ -25,41 +46,55 @@ extern "C" {
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
         //initialize window
-        GLFWwindow* window = glfwCreateWindow(1280, 720, "Ratworld", NULL, NULL);
-        if (!window)
+        
+        w =  glfwCreateWindow(1280, 720, "Ratworld", NULL, NULL);
+        if (!w)
         {
             std::cout << "fail" << std::endl;
             glfwTerminate();
             return(-1);
         }
 
-        glfwMakeContextCurrent(window);
+        glfwMakeContextCurrent(w);
 
         glewExperimental = GL_TRUE;
 
 
         //set background color
         glClearColor(.3f, .3f, .3f, 1.0f);
+
+        glfwSetKeyCallback(w, HandleKeyInput);
         //loop till window close
-        while (!glfwWindowShouldClose(window))
+        /*while (!glfwWindowShouldClose(window))
         {
             //accept inputs
             glfwPollEvents();
             //clears buffer
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             glfwSwapBuffers(window);
-        }
+        }*/
 
-
-        glfwDestroyWindow(window);
-        glfwTerminate();
+        
         return 1;
-
     }
 
-    __declspec(dllexport) void HandleKeyInput(GLFWwindow* w, int key, int status, int action, int mods) {
+    
 
+    __declspec(dllexport) BSTR  GetInput()
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glfwSwapBuffers(w);
+        
+        glfwPollEvents();
+        
 
+        //yes this is required
+        char* char_ = new char[lastkey.length() +1];
+
+        strcpy(char_, lastkey.c_str());
+        std::cout << "c++ output " << char_ << std::endl;
+
+        return SysAllocString(CA2W(char_));
     }
 }
 
--- main.cs
+++ main.cs
@@ -7,12 +7,21 @@ class Class1
 {
     [DllImport("ratgraphics.dll", CallingConvention = CallingConvention.Cdecl)]
     public static extern int rat();
+    [DllImport("ratgraphics.dll", CallingConvention = CallingConvention.Cdecl)]
+    [return: MarshalAs(UnmanagedType.BStr)]
+    public static extern string GetInput();
 
     static void Main(string[] args)
     {
         
         Console.WriteLine("rats");
         rat();
+
+        while(true)
+        {
+            
+            Console.WriteLine(GetInput());
+        }
 
     }
 
