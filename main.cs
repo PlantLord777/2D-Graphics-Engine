a61c6fec559e8b6f894464c93dcfc6c7cfd2bbf8
PlantLord777 <johnjohnson55687@gmail.com>
2024-04-01 11:59:14 -0500

    added comments

--- main.cs
+++ main.cs
@@ -80,8 +80,10 @@ class LOOP
         string s = GetInput();
         s= s.Trim();
         string[] ss = s.Split(' ');
+        //65 is A
         if (s.Split(' ')[0]=="65"&&( s.Split(' ')[2] == "0" || s.Split(' ')[2] == "1"))
         {
+            //keeps within bounds
             if (currentposx >1) {
                 
                 currentposx -= 1;
@@ -89,6 +91,7 @@ class LOOP
                 MoveSnake(currentposx, currentposy);
             }
         }
+        //68 is D
         if (s.Split(' ')[0] == "68" &&( s.Split(' ')[2] == "0" || s.Split(' ')[2] == "1"))
         {
             if (currentposx < 18)
@@ -99,6 +102,7 @@ class LOOP
                 MoveSnake(currentposx, currentposy);
             }
         }
+        //83 is S
         if (s.Split(' ')[0] == "83" && (s.Split(' ')[2] == "0" || s.Split(' ')[2] == "1"))
         {
             if (currentposy >1)
@@ -109,6 +113,7 @@ class LOOP
                 MoveSnake(currentposx, currentposy);
             }
         }
+        //87 is W
         if (s.Split(' ')[0] == "87" && (s.Split(' ')[2] == "0" || s.Split(' ')[2] == "1"))
         {
             if (currentposy < 18)
@@ -124,18 +129,21 @@ class LOOP
         {
             Console.WriteLine(s);
             //stops repreated reading of same input
+            //commented out to keep snake moving
             //ResetInput();
         }
 
-        for (int r = 0; r < 20; r++)
+        //draw the map based on held value
+        //1 = green(snake) 2 = red (apple) 3 = black(wall)
+        for (int x = 0; x < 20; x++)
         {
-            for (int c = 0; c < 20; c++)
+            for (int y = 0; y < 20; y++)
             {
-                if (map[r,c] == 1)
+                if (map[x,y] == 1)
                     drawSquare((r-10) * .1f, (c-10) * .1f,.1f,0f, 1.0f, 0f);
-                if (map[r,c]==2)
+                if (map[x,y]==2)
                     drawSquare((r-10) * .1f, (c-10) * .1f,.1f, 1f, 0f, 0f);
-                if (map[r, c] == 3)
+                if (map[x, y] == 3)
                     drawSquare((r-10) * .1f, (c-10) * .1f,.1f, 0f, 0f, 0f);
             }
         }
@@ -145,60 +153,68 @@ class LOOP
 
     public static void createMap()
     {
-        
-        for(int r=0;r<20;r++)
+        //init map to 0
+        for(int x=0;x<20;x++)
         {
-            for(int c =0; c< 20; c++)
+            for(int y =0; y< 20; y++)
             {
                 map[r,c] = 0;
             }
         }
 
-
+        //set snake start pos to 2,4
         MoveSnake(2, 4);
-
-        for (int r = 0; r < 20; r++)
+        //adds walls
+        for (int x = 0; x < 20; x++)
         {
-            map[r, 0] = 3;
-            map[r, 19] = 3;
+            map[x, 0] = 3;
+            map[x, 19] = 3;
         }
-        for(int c =0; c<20;c++)
+        for(int y =0; y<20;y++)
         {
-            map[0, c] = 3;
-            map[19, c] = 3;
-        }        createApple();
+            map[0, y] = 3;
+            map[19, y] = 3;
+        }        
+        //create first apple
+        createApple();
     }
 
     public static void createApple()
     {
         var rand = new Random();
-        int r = rand.Next(11)+1;
-        int c = rand.Next(11)+1;
+        int x = rand.Next(11)+1;
+        int y = rand.Next(11)+1;
+        //loop until empty space found
         while (map[r, c] != 0)
         {
-             r = rand.Next(17) + 1;
-             c = rand.Next(17) + 1;
+             x = rand.Next(17) + 1;
+             y = rand.Next(17) + 1;
         }
-        map[r,c] = 2;
-        snakelength++;
+        map[x,y] = 2;
+       
     }
 
     public static void MoveSnake(int x, int y)
     {
+        //create new apple if on smae tile as apple
         if (map[x, y] == 2)
             eatApple();
 
+        //clear array of snake
         foreach (string s in snakepos)
         {
 
             map[Int32.Parse(s.Split(' ')[0]), Int32.Parse(s.Split(' ')[1])] = 0;
 
         }
+        //add new pos
         snakepos.Enqueue(x + " " + y);
+        //delete old spaces
         while(snakepos.Count > snakelength)
         {
             snakepos.Dequeue();
         }
+        //set position on map
         foreach(string s in snakepos)
         {
 
@@ -210,6 +226,7 @@ class LOOP
     public static void eatApple()
     {
         Console.WriteLine("Apple");
+        snakelength++;
         createApple();
     }
 }
